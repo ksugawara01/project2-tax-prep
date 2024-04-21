b60909bf9de141229dc63ae4fa247cfc54388b2e
Koudou Sugawara <ksugawara01@gmail.com>
2024-04-20 21:07:26 -0400

    add footer

---
+++ project2-tax-prep-frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+
+export default function Footer() {
+    return(
+        <div id='footer'>
+            <div >License Agreement | Privacy | Manage cookies | Security | Cobrowse | Give feedback</div>
+            <div>© 2024 Fake Tax Company</div>
+        </div>
+    )
+}
