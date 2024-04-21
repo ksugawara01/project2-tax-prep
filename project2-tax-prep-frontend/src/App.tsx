2431c69e7d54db0f139dcf99ea4fc36be95b9a8a
Koudou Sugawara <ksugawara01@gmail.com>
2024-04-20 21:05:21 -0400

    add header, footer, and routes

--- project2-tax-prep-frontend/src/App.tsx
+++ project2-tax-prep-frontend/src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+import Footer from './components/Footer/Footer';
+import LandingPage from './components/LandingPage/LandingPage';
+import TrussHeader from './components/TrussHeader/TrussHeader';
+import FinancialInformationForm from './components/FinancialInformationForm/FinancialInformationForm.tsx';
+import PersonalInformationForm from './components/PersonalInformationForm/PersonalInformationForm.tsx'
+import ReviewPage from './components/ReviewPage/ReviewPage.tsx';
+import ResultsPage from './components/ResultsPage/ResultsPage.tsx'
 
 export default function App() {
 
     return (
-        <>
-            <div>hello world</div>
-        </>
+        <div className='flex-column-center'>
+            <TrussHeader />
+            <Routes>
+                <Route path='/' element={<LandingPage />}/>
+                <Route path='/personal-information' element={<PersonalInformationForm />} />
+                <Route path='/financial-information' element={<FinancialInformationForm />} />
+                <Route path='/review' element={<ReviewPage />} />
+                <Route path='/results' element={<ResultsPage />} />
+            </Routes>
+            <Footer />
+        </div>
     )
 }
 
