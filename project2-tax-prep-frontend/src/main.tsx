627682ced8a8f0fbebc94f43574ca29c91c53135
Koudou Sugawara <ksugawara01@gmail.com>
2024-04-22 20:47:18 -0400

    add store and provider

--- project2-tax-prep-frontend/src/main.tsx
+++ project2-tax-prep-frontend/src/main.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store.ts'
 
 import '@trussworks/react-uswds/lib/uswds.css'
 import '@trussworks/react-uswds/lib/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
 )
