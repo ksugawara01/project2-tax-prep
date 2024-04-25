a68889a7e5760f75bda9c9d373121a318e681e05
Koudou Sugawara <ksugawara01@gmail.com>
2024-04-25 13:59:41 -0400

    add functionality to change languages using the language selector

--- project2-tax-prep-frontend/src/components/TrussHeader/TrussHeader.tsx
+++ project2-tax-prep-frontend/src/components/TrussHeader/TrussHeader.tsx
@@ -1,15 +1,31 @@
 import { Header, NavMenuButton, Title, PrimaryNav, Search, NavDropDownButton, Menu, LanguageSelector } from '@trussworks/react-uswds'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './TrussHeader.css'
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateCurrentLanguage } from '../../slices/currentLanguageSlice';
 
 export default function TrussHeader() {
 
     const { t } = useTranslation();
 
+    // get current language from the store
+    const currentLanguage = useSelector((store : any) => store.currentLanguage)
+    const dispatch = useDispatch();
+
     const [expanded, setExpanded] = useState(false);
 
     const onClick = (): void => setExpanded(prvExpanded => !prvExpanded);
+
+    const handleLanguageSwitch = () => {
+        dispatch(updateCurrentLanguage('en'))
+        
+    }
+
+        // Log financialInformation whenever it changes
+        useEffect(() => {
+          console.log('current language:', currentLanguage);
+      }, [currentLanguage]);
 
     const testItemsMenu = [
       <a href='#two' key='two' className='usa-nav__link'>
@@ -24,19 +40,19 @@ export default function TrussHeader() {
               {
                 attr: 'en',
                 label: 'English',
-                on_click: function Ga(){}
+                on_click: function Ga(){dispatch(updateCurrentLanguage({language: 'en'}))}
               },
               {
                 attr: 'es',
                 label: 'Español',
                 label_local: 'Spanish',
-                on_click: function Ga(){}
+                on_click: function Ga(){dispatch(updateCurrentLanguage({language: 'es'}))}
               },
               {
                 attr: 'jp',
                 label: '日本語',
                 label_local: 'Japanese',
-                on_click: function Ga(){}
+                on_click: function Ga(){dispatch(updateCurrentLanguage({language: 'jp'}))}
               }
           ]}
       />
