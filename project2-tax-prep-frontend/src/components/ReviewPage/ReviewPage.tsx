f12053f6543cbfe3a6ff6d66ce77e1283d7dad5d
Koudou Sugawara <ksugawara01@gmail.com>
2024-04-29 21:29:10 -0400

    remove placeholder information and add data from redux store

--- project2-tax-prep-frontend/src/components/ReviewPage/ReviewPage.tsx
+++ project2-tax-prep-frontend/src/components/ReviewPage/ReviewPage.tsx
@@ -3,9 +3,15 @@ import { Link, useNavigate } from 'react-router-dom'
 import './ReviewPage.css'
 import TrussStepIndicator from '../TrussStepIndicator/TrussStepIndicator';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 
  export default function ReviewPage() {
+
+    // Select personal information from the store
+    const personalInformation = useSelector((store : any) => store.personalInformation)
+
+    const financialInformation = useSelector((store : any) => store.financialInformation)
 
     const { t } = useTranslation();
 
@@ -19,6 +25,36 @@ import { useTranslation } from 'react-i18next';
         navigate('/results')
     }
 
+    const handlePersonalEdit = () => {
+        navigate('/personal-information')
+    }
+
+    const handleFinancialEdit = () => {
+        navigate('/financial-information')
+    }
+
+    // Converts the isMarried boolean into a string for the information review
+    const maritalStatus = (isMarried : any) => {
+        if (isMarried === '') {
+            return '';
+        } else if (isMarried === true) {
+            return 'Married';
+        } else if (isMarried === false) {
+            return 'Single';
+        }
+    }
+
+    // Converts the isStandardDeduction boolean into a string for the information review
+    const deductionType = (isStandardDeduction : any) => {
+        if (isStandardDeduction === '') {
+            return '';
+        } else if (isStandardDeduction === true) {
+            return 'Standard';
+        } else if (isStandardDeduction === false) {
+            return 'Itemized';
+        }
+    }
+
     return(
         <div className='flex-column-center'>
             <TrussStepIndicator personalStatus='complete' financialStatus='complete' reviewStatus='current'/>
@@ -26,39 +62,38 @@ import { useTranslation } from 'react-i18next';
             <Table bordered={false} fullWidth>
                 <thead>
                     <tr>
-                        <th>{t('review.personalInformation')} <Button className='review-edit-button' type='button'>{t('button.edit')}</Button></th>
-
+                        <th>{t('review.personalInformation')} <Button className='review-edit-button' type='button' onClick={handlePersonalEdit} >{t('button.edit')}</Button></th>
                     </tr>
 
                 </thead>
                 <tbody>
                     <tr>
                         <td>{t('personalInformationForm.firstName')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.firstName}</td>
                     </tr>
                     <tr>
                         <td>{t('personalInformationForm.lastName')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.lastName}</td>
                     </tr>
                     <tr>
                         <td>{t('personalInformationForm.streetAddress')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.streetAddress}</td>
                     </tr>
                     <tr>
                         <td>{t('personalInformationForm.city')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.city}</td>
                     </tr>
                     <tr>
                         <td>{t('personalInformationForm.state')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.state}</td>
                     </tr>
                     <tr>
                         <td>{t('personalInformationForm.zip')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.zip}</td>
                     </tr>
                     <tr>
                         <td>{t('personalInformationForm.ssn')}</td>
-                        <td>Placeholder</td>
+                        <td>{personalInformation.ssn}</td>
                     </tr>
 
                 </tbody>
@@ -68,34 +103,34 @@ import { useTranslation } from 'react-i18next';
             <Table bordered={false} fullWidth>
                 <thead>
                     <tr>
-                        <th>{t('review.financialInformation')} <Button className='review-edit-button' type='button'>{t('button.edit')}</Button></th>
+                        <th>{t('review.financialInformation')} <Button className='review-edit-button' type='button' onClick={handleFinancialEdit} >{t('button.edit')}</Button></th>
                     </tr>
 
                 </thead>
                 <tbody>
                     <tr>
                         <td>{t('financialInformationForm.incomeW2')}</td>
-                        <td>Placeholder</td>
+                        <td>{financialInformation.incomeW2}</td>
                     </tr>
                     <tr>
                         <td>{t('financialInformationForm.withholdingsW2')}</td>
-                        <td>Placeholder</td>
+                        <td>{financialInformation.withholdingsW2}</td>
                     </tr>
                     <tr>
                         <td>{t('financialInformationForm.income1099')}</td>
-                        <td>Placeholder</td>
+                        <td>{financialInformation.income1099}</td>
                     </tr>
                     <tr>
                         <td>{t('financialInformationForm.deductions')}</td>
-                        <td>Placeholder</td>
+                        <td>{financialInformation.deductions}</td>
                     </tr>
                     <tr>
                         <td>{t('review.maritalStatus')}</td>
-                        <td>Placeholder</td>
+                        <td>{maritalStatus(financialInformation.isMarried)}</td>
                     </tr>
                     <tr>
                         <td>{t('review.deductionType')}</td>
-                        <td>Placeholder</td>
+                        <td>{deductionType(financialInformation.isStandardDeduction)}</td>
                     </tr>
 
                 </tbody>
