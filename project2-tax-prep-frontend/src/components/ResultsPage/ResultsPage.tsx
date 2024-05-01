f1e093807fbe4f5e52b1186ec938ad139cd4b937
Koudou Sugawara <ksugawara01@gmail.com>
2024-05-01 17:39:36 -0400

    implement logic and backend connectivity

--- project2-tax-prep-frontend/src/components/ResultsPage/ResultsPage.tsx
+++ project2-tax-prep-frontend/src/components/ResultsPage/ResultsPage.tsx
@@ -6,15 +6,78 @@ import Confetti from "react-dom-confetti"
 import TrussStepIndicator from "../TrussStepIndicator/TrussStepIndicator"
 import { useTranslation } from 'react-i18next'
 
+import taxCalculatorService from '../../services/tax-calculator'
+import { useSelector } from 'react-redux'
+
 export default function ResultsPage() {
+
+    const [taxesOwed, setTaxesOwed] = useState(1);
+    const [returnAmount, setReturnAmount] = useState(0);
+    const [taxableIncome, setTaxableIncome] = useState(0);
+
+    // Select personal information from the store
+    const financialInformation = useSelector((store : any) => store.financialInformation)
 
     const [isConfettiActive, setIsConfettiActive] = useState(false)
 
     const { t } = useTranslation();
 
-    // Start confetti animation on mount
+    const totalIncome = Number(financialInformation.incomeW2) + Number(financialInformation.income1099);
+
+    const calculateReturn = (taxableIncome : number) => {
+        if (financialInformation.isMarried) {
+            taxCalculatorService.getMarriedFilerTax(taxableIncome)
+            .then((taxesOwed) => {
+
+                const returnAmount = financialInformation.withholdingsW2 - taxesOwed;
+
+                if (returnAmount >= 0) {
+                    // Start confetti animation
+                    setIsConfettiActive(true)
+                }
+
+                setTaxesOwed(taxesOwed)
+                setReturnAmount(returnAmount);
+            })
+        } else {
+            taxCalculatorService.getSingleFilerTax(taxableIncome)
+            .then((taxesOwed) => {
+
+                const returnAmount = financialInformation.withholdingsW2 - taxesOwed;
+                if (returnAmount >= 0) {
+                    // Start confetti animation
+                    setIsConfettiActive(true)
+                } 
+
+                setTaxesOwed(taxesOwed)
+                setReturnAmount(returnAmount);
+            })
+        }
+
+    }
+
     useEffect(() => {
-        setIsConfettiActive(true)
+        console.log('financial information', financialInformation)
+
+        // Standard deduction calculation
+        if (financialInformation.isStandardDeduction) {
+            const deductions = 13850; // the standard deduction for 2023 taxes is $13850
+            let taxable = totalIncome - deductions;
+            if (taxable < 0) {taxable = 0}
+
+            setTaxableIncome(taxable)
+            calculateReturn(taxable);
+            console.log('total income', totalIncome)
+        // Itemized Deduction calculation 
+        } else {
+            const deductions = financialInformation.deductions;
+            let taxable = totalIncome - deductions;
+            if (taxable > 0) {taxable = 0}
+
+            setTaxableIncome(taxable)
+            calculateReturn(taxable);
+        }
+
     }, [])
 
     // Confetti Settings
@@ -38,13 +101,13 @@ export default function ResultsPage() {
             <Confetti active={ isConfettiActive } config={ config }/>
             <div id='result-congratulations'>{t('results.congratulations')}</div>
             <div className='result-categories'>{t('results.taxableIncome')}</div>
-            <div>placeholder</div>
+            <div>${taxableIncome.toFixed(2)}</div>
             <div className='result-categories'>{t('results.taxesOwed')}</div>
-            <div>placeholder</div>
+            <div>${taxesOwed.toFixed(2)}</div>
             <div className='result-categories'>{t('results.withholdings')}</div>
-            <div>placeholder</div>
+            <div>${Number(financialInformation.withholdingsW2).toFixed(2)}</div>
             <div className='result-categories'>{t('results.federalRefund')}</div>
-            <div id='refund-amount'>$2,452</div>
+            <div id='refund-amount'>${returnAmount.toFixed(2)}</div>
         </>
     )
 }
